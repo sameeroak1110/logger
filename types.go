be4ce5f3098586bc301c2f087702268cf2429702
sameeroak1110 <sameeroak1110@gmail.com>
2022-06-17 14:48:13 +0530

    Removed FileNamePrefix from config. It's always server.log

--- types.go
+++ types.go
@@ -25,7 +25,6 @@ type logmessage struct {
 
 type LogConfig struct {
 	SrcBaseDir      string `json:"srcBaseDir"`       // $PWD
-	FileNamePrefix  string `json:"fileNamePrefix"`   // server.log
 	FileSize        int    `json:"fileSize"`         // 20971520 (20MB)
 	MaxFilesCnt     int    `json:"maxFilesCnt"`      // 10
 	DefaultLogLevel string `json:"defaultLogLevel"`  // DEBUG
